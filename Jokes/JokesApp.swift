719c080bc95125a2627b393f7282466831fd3126
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-04-14 14:34:30 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Oliver Finlayson on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
