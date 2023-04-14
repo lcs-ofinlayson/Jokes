6463080605b57aeb9b39f8c6d1f5f09007aee367
Oliver Finlayson <ofinlayson@lcs.on.ca>
2023-04-14 14:44:06 -0400

    Added joke setup and nav title.

---
+++ Jokes/Views/JokeView.swift
@@ -0,0 +1,29 @@
+//
+//  JokeView.swift
+//  Jokes
+//
+//  Created by Oliver Finlayson on 2023-04-14.
+//
+
+import SwiftUI
+
+struct JokeView: View {
+    var body: some View {
+        NavigationView{
+            
+            VStack{
+                Text("You see, mountains aren't just funny.")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+            }
+            .navigationTitle("Random Jokes")
+            
+        }
+    }
+}
+
+struct JokeView_Previews: PreviewProvider {
+    static var previews: some View {
+        JokeView()
+    }
+}
